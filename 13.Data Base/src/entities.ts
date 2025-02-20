324b58d98852df0f4b1c6ee42a72022527b03a50
Jerots <jeronimotsv@gmail.com>
2025-02-20 13:55:33 +0100

    Add actualizacion de src en 13.db

--- 13.Data Base/src/entities.ts
+++ 13.Data Base/src/entities.ts
@@ -1,1 +1,11 @@
-s;
+import type { RowDataPacket } from 'mysql2/promise';
+
+// interface Category extends mysql.RowDataPacket {
+//     id: number;
+//     name: string;
+// };
+
+export type Generes = {
+    id: number;
+    name: string;
+} & RowDataPacket;
