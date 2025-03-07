49c49d404551bd3f122476b29cbf284518f2c959
Jerots <jeronimotsv@gmail.com>
2025-03-07 13:45:50 +0100

    Add clase 7-3-25

--- 15.Films.prisma/src/router/films.router.ts
+++ 15.Films.prisma/src/router/films.router.ts
@@ -2,8 +2,9 @@ import { Router } from 'express';
 import { FilmsController } from '../controllers/films.controller.js';
 import createDebug from 'debug';
 import { AuthInterceptor } from '../middleware/auth.interceptor.js';
+import { Role } from '@prisma/client';
 
-const debug = createDebug('films:router:films');
+const debug = createDebug('movies:router:films');
 
 export const createFilmsRouter = (
     authInterceptor: AuthInterceptor,
@@ -12,26 +13,24 @@ export const createFilmsRouter = (
     debug('Ejecutando createFilmsRouter');
 
     const filmsRouter = Router();
-    filmsRouter.get(
+    filmsRouter.get('/', filmsController.getAll);
+    filmsRouter.get('/:id', filmsController.getById);
+    filmsRouter.post(
         '/',
         authInterceptor.authenticate,
-        authInterceptor.isAdmin,
-        filmsController.getAll,
+        authInterceptor.hasRole(Role.EDITOR),
+        filmsController.create,
     );
-    filmsRouter.get(
-        '/:id',
-        authInterceptor.authenticate,
-        filmsController.getById,
-    );
-    filmsRouter.post('/', authInterceptor.authenticate, filmsController.create);
     filmsRouter.patch(
         '/:id',
         authInterceptor.authenticate,
+        authInterceptor.hasRole(Role.EDITOR),
         filmsController.update,
     );
     filmsRouter.delete(
         '/:id',
         authInterceptor.authenticate,
+        authInterceptor.hasRole(Role.EDITOR),
         filmsController.delete,
     );
     return filmsRouter;
